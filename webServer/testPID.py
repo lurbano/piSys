5661ec670c48643cc8933eb810b0c7db817fa5f5
lurbano <lurbano@earthsciweb.org>
2021-03-17 16:20:19 -0500

    test sleep

--- webServer/testPID.py
+++ webServer/testPID.py
@@ -1,7 +1,10 @@
+import time
 from uPID import *
 from sensor_T import *
 
 sensor = sensor_T()
 pid = uPID(sensor)
 
-print(pid.read())
+for i in range(10):
+    print(pid.read())
+    time.sleep(1)
