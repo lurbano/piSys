3a3b8f6d853e0ac9964c18ae9992a9837b1b49df
lurbano <lurbano@earthsciweb.org>
2021-03-18 12:16:11 -0500

    Styling

--- webServer/static/ws-client.js
+++ webServer/static/ws-client.js
@@ -85,9 +85,9 @@ $(document).ready(function(){
             $("#pidFlag").html("Off")
           }
 
-          $("#sensor_measure").html(sData.x + " °C");
+          $("#pid_measure").html(sData.x + " °C");
           let now = new Date();
-          $("#sensor_time").html(now.toString().split(" GMT")[0]);
+          $("#pid_time").html(now.toString().split(" GMT")[0]);
 
         }
         //PID (END)
